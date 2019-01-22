a00b8ca48a3fc2fc6636bcb798d39a5937c03729
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-22 15:47:24 -0700

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../main.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Dylan's swapibox</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
