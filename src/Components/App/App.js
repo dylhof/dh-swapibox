cc11ebef22190f0e8daac3ecb31ffd6fcbc85c62
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-23 16:41:31 -0700

    Add fetch call to get a random film and set state for crawl

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import '../../main.scss';
+import Crawl from '../Crawl/Crawl.js'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      crawl: ''
+    }
+  }
+
+  componentDidMount = () => {
+    const episode = Math.ceil(Math.random() * 7)
+    const url = `https://swapi.co/api/films/${episode}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(film => this.setState({ crawl: film.opening_crawl }))
+  }
+
   render() {
+    const { crawl } = this.state;
     return (
-      <div className="App">
-        <h1>Dylan's swapibox</h1>
+      <div className='App'>
+        <Crawl crawl={crawl} />
       </div>
     );
   }
