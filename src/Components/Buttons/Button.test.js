b06aff0c27fd5293bd4cef176ccfec8caffbe228
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-23 17:40:51 -0700

    Add button snapshot test

--- src/Components/Buttons/Button.test.js
+++ src/Components/Buttons/Button.test.js
@@ -3,5 +3,13 @@ import { shallow } from 'enzyme';
 import Buttons from './Buttons';
 
 describe('Buttons', () => {
-  
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <Buttons /> 
+    )
+  })
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
 })
