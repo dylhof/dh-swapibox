88497607578e4eabd64d7976f8a5da09f1e73625
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-26 15:12:23 -0700

    Break apart people planets and vehicles  into multiple functions

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -37,67 +37,95 @@ class App extends Component {
     if (!this.state.people.length) {
       const peopleDataObject = await api.fetchData('https://swapi.co/api/people')
       const peopleData = peopleDataObject.results
-      const people = await Promise.all(peopleData.map(async (person) => {
-        const homeworld = await api.fetchData(person.homeworld)
-        const species = await api.fetchData(...person.species)
-        const personData = { 
-          name: person.name, 
-          homeworld: homeworld.name, 
-          species: species.name, 
-          population: homeworld.population, 
-          category: 'person'}
-        return personData;
-      }))
-     await this.setState({ people, currentView: 'people' });
+      const people = await this.peopleMap(peopleData);
+      await this.setState({ people, currentView: 'people' });
     } else {
       this.setState({ currentView: 'people' })
     }
+  }
+
+  peopleMap = async (peopleData) => {
+    const people = await Promise.all(peopleData.map(async (person) => {
+      const homeworld = await this.personHomeworldFetch(person)
+      const species = await this.personSpeciesFetch(person);
+      return {
+        name: person.name,
+        homeworld: homeworld.name,
+        species: species.name,
+        population: homeworld.population,
+        category: 'person'
+      }
+    }))
+    return people
+  }
+
+  personHomeworldFetch = async (person) => {
+    const homeworld = await api.fetchData(person.homeworld);
+    return homeworld;
+  }
+
+  personSpeciesFetch = async (person) => {
+    const species = await api.fetchData(...person.species);
+    return species;
   }
 
   makePlanets = async () => {
     if(!this.state.planets.length) {
       const planetsDataObject = await api.fetchData('https://swapi.co/api/planets')
       const planetsData = planetsDataObject.results
-      const planets = await Promise.all(planetsData.map(async (planet) => {
-        const residents = await Promise.all(planet.residents.map(async (residentURL) => {
-          const residentData = await api.fetchData(residentURL);
-          const resident = residentData.name
-          return resident
-        }))
-        const planetData = { 
-          name: planet.name, 
-          terrain: planet.terrain, 
-          population: planet.population, 
-          climate: planet.climate, 
-          residents: residents, 
-          category: 'planet'}
-        return planetData;
-      }))
+      const planets = await this.planetMap(planetsData)
       await this.setState({ planets, currentView: 'planets'})
     } else {
       this.setState({ currentView: 'planets' })
     }
+  }
+
+  planetMap = async (planetsData) => {
+    const planets = await Promise.all(planetsData.map(async (planet) => {
+      const residents = await this.planetResidentMap(planet);
+      return {
+        name: planet.name, 
+        terrain: planet.terrain, 
+        population: planet.population, 
+        climate: planet.climate, 
+        residents: residents, 
+        category: 'planet'
+      }
+    }))
+    return planets
+  }
+
+  planetResidentMap = async (planet) => {
+    const residents = await Promise.all(planet.residents.map(async (residentURL) => {
+      const residentData = await api.fetchData(residentURL);
+      const resident = residentData.name
+      return resident
+    }))
+    return residents
   }
   
   makeVehicles = async () => {
     if(!this.state.vehicles.length) {
       const vehiclesDataObject = await api.fetchData('https://swapi.co/api/vehicles')
       const vehiclesData = vehiclesDataObject.results
-      console.log(vehiclesData)
-      const vehicles = vehiclesData.map((vehicle) => {
-        return { 
-          name: vehicle.name, 
-          model: vehicle.model, 
-          class: vehicle.class, 
-          passengers: vehicle.passengers, 
-          category: 'vehicle'}
-      })
+      const vehicles = this.vehiclesMap(vehiclesData)
       await this.setState({ vehicles, currentView: 'vehicles'})
     } else {
       this.setState({ currentView: 'vehicles' })
     }
   }
 
+  vehiclesMap = (vehiclesData) => {
+    const vehicles = vehiclesData.map((vehicle) => {
+      return { 
+        name: vehicle.name, 
+        model: vehicle.model, 
+        class: vehicle.class, 
+        passengers: vehicle.passengers, 
+        category: 'vehicle'}
+    })
+    return vehicles
+  }
 
   render() {
     const { currentView, film } = this.state;
--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -11,47 +11,6 @@ describe('App', () => {
     ReactDOM.unmountComponentAtNode(div);
   });
   
-  describe('componentDidMount', () => {
-    let mockEpisode;
-    let mockUrl;
-    let wrapper;
-    let mockFilm;
-    let mockMathCeil;
-
-    beforeEach(() => {
-      mockEpisode = 1;
-      mockUrl = `https://swapi.co/api/films/${mockEpisode}`;
-      mockFilm = { opening_crawl: 'It was a dark time for the rebellion.'};
-      wrapper = shallow(
-        <App /> 
-      );
-      Math.Ceil = jest.fn().mockImplementation(() => mockEpisode)
-      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-        json: Promise.resolve(mockFilm),
-        ok: true,
-      }));
-    })
-
-    it('call fetch with the correct parameters', () => {
-      //setup
-      //execution
-      wrapper.instance().componentDidMount()
-      //expectation
-      expect(window.fetch).toHaveBeenCalledWith(mockUrl)
-    })
-
-    it('should set state as the opening crawl', () => {
-      //setup
-      const expectedState = 'It was a dark time for the rebellion.'
-      expect(wrapper.state('crawl')).toEqual('');
-      //execution
-      wrapper.instance().componentDidMount()
-        .then(() => {
-          //expectation
-            expect(wrapper.state('crawl')).toEqual(expectedState)
-        }) 
-    })
-
-  })
+  
 
 })
