cef1b8f02359268bfa0724525725927ad4983654
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-01-26 23:38:06 -0700

    Add annimation and style to crawl

--- src/Components/Crawl/Crawl.js
+++ src/Components/Crawl/Crawl.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const Crawl = ({ crawl, title, date }) => {
-  return (
-    <div>
-      <p className='crawl'>{crawl}</p>
-      <p className='crawl'>{title}</p>
-      <p className='crawl'>{date}</p> 
-    </div>
+  return ( 
+    <section className='crawl-section'>
+      <div className='crawl-div'>
+        <p className='crawl-title'>{title}</p>
+        <p className='crawl'>{date}</p> 
+        <p className='crawl'>{crawl}</p>
+        <p className='choose-category'>Choose A Category...</p>
+      </div>
+    </section>
   )
 }
 
